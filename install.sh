b925543dac78517d4a9030b1dc403d1300ab6e3e
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-02-01 14:12:54 +0100

    [fix] install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "creating new folders"
 mkdir data
+mkdir ./src/static/data
 echo "fetching latest toflit18 data"
 curl -o toflit18_bdd.zip "https://raw.githubusercontent.com/medialab/toflit18_data/master/base/bdd%20courante.csv.zip"
 unzip toflit18_bdd.zip -d "data"
@@ -8,7 +9,8 @@ mv "data/bdd courante.csv" "data/toflit18_all.csv"
 echo "fetching latest toflit18 schema"
 curl -o data/toflit18_sources_schema.json "https://raw.githubusercontent.com/medialab/toflit18_data/master/csv_sources_schema.json"
 echo "fetching latest navigo pointcalls data"
-curl -o data/navigo_all_pointcalls_1789.json "data.portic.fr/api/pointcalls/?date=1789"
+curl -o data/navigo_all_pointcalls_1789.csv "data.portic.fr/api/pointcalls/?date=1789&format=csv"
+curl -o data/navigo_all_pointcalls_1787.csv "data.portic.fr/api/pointcalls/?date=1787&format=csv"
 echo "fetching latest navigo flows data"
 curl -o data/navigo_all_flows_1789.csv "data.portic.fr/api/rawflows/?date=1789&format=csv"
 curl -o data/navigo_all_flows_1787.csv "data.portic.fr/api/rawflows/?date=1787&format=csv"
@@ -19,4 +21,4 @@ curl -o data/portic_flows_descriptions.json "http://data.portic.fr/api/fieldname
 echo "install NodeJS dependencies"
 npm i
 echo "process data with Python scripts"
-cd ./scripts && for f in ./*.py; do python "$f"; done
+cd ./scripts && for f in *.py; do python3 "$f"; echo "execute python script $f"; done
